76489f499a6bbc93f415d0048277ea2f096c9322
Sarah Wood <photosbysarahwood@gmail.com>
2022-04-05 19:57:55 -0400

    tried some stuff

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,15 @@
+var first_name = "Sarah";
+var last_name = " Wood";
+var age = 22;
+var favorite_food = "pickles";
 
+console.log(
+  `My name is ${first_name}. I am ${age} years old and I love to eat ${favorite_food}.`
+);
+
+var num = 2;
+var result = num + num;
+var full_name = first_name + last_name;
+
+console.log(result);
+console.log(full_name);
